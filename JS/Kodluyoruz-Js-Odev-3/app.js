28c730836973590346c63eddba19bdad64b0ee40
Ozan <ozan.yasin97@gmail.com>
2022-05-03 17:06:08 +0300

    kodluyoruz-js-odev-3 updated

--- JS/Kodluyoruz-Js-Odev-3/app.js
+++ JS/Kodluyoruz-Js-Odev-3/app.js
@@ -72,3 +72,29 @@ const menu = [
     desc: `Red bean paste dessert, serving with honey.`,
   },
 ];
+
+// Selectors
+
+const sectionCenter = document.querySelector(".section-center");
+
+window.addEventListener("DOMContentLoaded", function () {
+  let displayMenu = menu.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+              <img src=${item.img} class="photo" alt=${item.title} />
+              <div class="item-info">
+                <header>
+                  <h4>${item.title}</h4>
+                  <h4 class="price">${item.price}</h4>
+                </header>
+                <p class="item-text">
+                  ${item.desc}
+                </p>
+              </div>
+            </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+  sectionCenter.innerHTML = displayMenu;
+});
